a64c63343f71aab7ad5aba869b5aefd0e4a7ed79
Ozanne Alexander <alexanoz@outlook.com>
2022-12-07 18:51:01 -0500

    adjusted button styling and forecast card placement on application

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,8 +1,8 @@
-// API key 
+// Variable Declaration  
 var apiKey = "71d7a3ac04487b24c5d8fe8c53a8bf95"
 
 // user input
 
 //local storage setItem
-$(".cityBtn")
+$(".searchBtn")
 // local storage getItem
