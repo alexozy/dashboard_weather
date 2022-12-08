41b4dbde66b29213d41658b53c185ae45cf01ef1
Ozanne Alexander <alexanoz@outlook.com>
2022-12-08 14:12:51 -0500

    set day vars and cleaned up html structure

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,8 +1,26 @@
 // Variable Declaration  
-var apiKey = "71d7a3ac04487b24c5d8fe8c53a8bf95"
+var apiKey = "71d7a3ac04487b24c5d8fe8c53a8bf95";
 
-// user input
+// days vars (is there a way for me to create the elements instead of having them already set up in html?)
+var currentDay = document.getElementById("toDay")
+var firstDay = document.getElementById("day1")
+var secondDay = document.getElementById("day2")
+var thirdDay = document.getElementById("day3")
+var fourthDay=document.getElementById("day4")
+var fifthDay=document.getElementById("day5")
 
-//local storage setItem
-$(".searchBtn")
+
+// user SEARCH BOX input placed into local storage setItem
+// $(".searchCity").each(function (i, btn){
+//     $(btn).on("click", function (){
+//         var value = $(this).siblings(".form-control").val();
+//         console.log(value)
+//         var key = $(this).parent().attr("id");
+//         localStorage.setItem(key,value)
+//     })
+// });
 // local storage getItem
+
+// search history to append results
+
+//API retrieval & display of data
