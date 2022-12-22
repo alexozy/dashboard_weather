9505eb46f902117a74d4c2a94b9a11500ce00b05
Ozanne Alexander <alexanoz@outlook.com>
2022-12-22 01:26:23 -0500

    fixed api call; created fetchAttempt function to properly retrieve API call data

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,9 +1,69 @@
-var apiKey = '71d7a3ac04487b24c5d8fe8c53a8bf95';
+// Variable Declaration
+var apiKey = "71d7a3ac04487b24c5d8fe8c53a8bf95";
+var cityInput = document.getElementById('searchedC').value;
 
-// This is Global localStorage
-// // JSON.parse converts string back to array | short hand if statement for empty array = ||[]
-//you want to getItem first, so the old stuff is saved (did this globally); you can also do within the function but locally is easier for the code
+
+
+// Global localStorage
+// JSON.parse converts string back to array | short hand if statement for empty array = ||[]
+//you want to getItem first, so the old stuff is saved (did this globally); you can also do within the function but locally is easier for the code.
 var searchedHistory = JSON.parse(localStorage.getItem("searched")) || [];
+
+// days vars (is there a way for me to create the elements instead of having them already set up in html?)
+var currentDay = document.getElementById("toDay");
+var firstDay = document.getElementById("day1");
+var secondDay = document.getElementById("day2");
+var thirdDay = document.getElementById("day3");
+var fourthDay = document.getElementById("day4");
+var fifthDay = document.getElementById("day5");
+
+
+// search button append; strategy: grab local, go through it, display it
+var displaySearchHistory = function () {
+        // clearing saved history before adding something to it
+        $(".saveHist").html('');
+        for (i = 0; i < searchedHistory.length; i++) {
+                //grabbing from an array using variable[i]
+                var city = searchedHistory[i]
+                var button = $("<button>").addClass("btn btn-primary btn-sm").text(city)
+                $(".saveHist").append(button)
+        }
+};
+
+// local storage clear
+var clearLocalStorage = function () {
+        localStorage.removeItem("searched");
+        $(".saveHist").html('');
+        searchedHistory = [];
+}
+$("#clearH").on('click', clearLocalStorage);
+
+//API retrieval & display of data
+// lat/lon https://api.openweathermap.org/data/2.5/weather?q=atlanta&units=imperial&appid=71d7a3ac04487b24c5d8fe8c53a8bf95
+
+let WeatherStuff = {
+        fetchAttempt: function (city){
+        var currentDay = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=71d7a3ac04487b24c5d8fe8c53a8bf95" 
+        fetch(currentDay).then(function (response) {
+                
+        return response.json()
+        
+        }).then(function (data) {
+                var { name } = data;
+                var { icon, description } = data.weather[0];
+                var { temp, humidity } = data.main;
+                var { speed } = data.wind;
+                var { lon, lat } = data.coord;
+                console.log(name, icon, temp, speed, lon, lat);
+
+        });
+},
+
+
+        search: function () {
+                this.fetchAttempt(document.querySelector("#searchedC").value)
+        }
+};
 
 // target search button; html button #addCities
 var searched = document.getElementById("searchedC");
@@ -18,59 +78,35 @@ $("form").submit(function (event) {
         localStorage.setItem("searched", JSON.stringify(searchedHistory));
         console.log(searchedCity)
         displaySearchHistory()
+        WeatherStuff.search();
 });
 
-// search button append; strategy: grab local, go through it, display it
-var displaySearchHistory = function () {
-    // clearing saved history before adding something to it
-    $(".saveHist").html('');
-    for (i = 0; i < searchedHistory.length; i++) {
-            //grabbing from an array using variable[i]
-            var city = searchedHistory[i]
-            var button = $("<button>").addClass("btn btn-primary btn-sm").text(city)
-            $(".saveHist").append(button)
-    }
-};
+// create an if to target a specific time// would be
 
-// local storage clear
-var clearLocalStorage = function () {
-    localStorage.removeItem("searched");
-    $(".saveHist").html('');
-    searchedHistory = [];
-}
-$("#clearH").on('click', clearLocalStorage);
 
-// API function for retrieval & display of requested the data:
+// // putting information on my cards
+//         // pulling info from the API/JSON page
+//         var {name} = data;
+//         var {speed} = data.wind;
+//         var {temp, humidity} = data.main;
+//         var {lon,lat} = data.coord;
+//         console.log(name, speed)
 
-function weatherStuff (event){
-    event.preventDefault()
-// save what user searches to cityInput, allows APIs to use, and will push to the array to store in local
-    var cityInput = document.getElementById('searchCity').value;
-    searchedHistory.push(cityInput);
-    localStorage.setItem('lsCities', JSON.stringify(searchedHistory));
-    console.log(localStorage);
+//         //querySelector (string concat within query selectors)
+//         var cities = (city)
+//         console.log (cities)
+//         $(".card-header" + cities).text
 
-// fetch weather
+//         var lat = data.coord.lat
+//         var lon = data.coord.lon
+//         var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&units=imperial&appid=71d7a3ac04487b24c5d8fe8c53a8bf95')
-.then(function(response){
-    return response.json ()
-})
-.then (function(data){
-    for (i=2; i <=6; i++){
-        var day = data.list [(i-2)* 8];
-        var degrees = document.querySelector('.degrees'+i);
-        // placing on the card
-        degrees.innerHTML = day.main.temp;
-        var humidity = document.querySelector('.humidity'+i);
-        humidity.innerHTML = day.main.humidity;
-        var date = document.querySelector('.date'+ i);
-        date.innerHTML = day.dt_txt;
-    }
-})
-
-};
-
-// you need to call your function!
-var searchProcess = document.getElementById('letsGo');
-searchProcess.addEventListener('submit', weatherStuff)
+//         fetch(fiveDay).then(function (response) {
+//                 return response.json()
+//         }).then(function (data) {
+//                 // If statement goes here for specific time
+//                 console.log(data)
+//                 var day = 1
+//                 // forLoop
+//                 $(".temp" + day).text
+//         })
